356ce2c7915057ef77a592ab091bd12ed1c3a484
forcesmazarine <forces.mazarine@gmail.com>
2025-10-29 16:24:01 +0300

    1

--- js/js.js
+++ js/js.js
@@ -3,44 +3,44 @@ let lastScrollY = window.scrollY;
 const tabs = document.querySelector('.tabs');
 
 if (tabs) {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > lastScrollY) {
-      // نازل لتحت → اخفي التابات
-      tabs.classList.add('hidden');
-    } else {
-      // طالع لفوق → أظهر التابات
-      tabs.classList.remove('hidden');
-    }
-    lastScrollY = window.scrollY;
-  });
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > lastScrollY) {
+            // نازل لتحت → اخفي التابات
+            tabs.classList.add('hidden');
+        } else {
+            // طالع لفوق → أظهر التابات
+            tabs.classList.remove('hidden');
+        }
+        lastScrollY = window.scrollY;
+    });
 }
 
 // 🔹 التحكم فى المربع الحواري (Popup)
 function setupPopup(openId, closeId, popupId, overlayId) {
-  const openBtn = document.getElementById(openId);
-  const closeBtn = document.getElementById(closeId);
-  const popup = document.getElementById(popupId);
-  const overlay = document.getElementById(overlayId);
+    const openBtn = document.getElementById(openId);
+    const closeBtn = document.getElementById(closeId);
+    const popup = document.getElementById(popupId);
+    const overlay = document.getElementById(overlayId);
 
-  openBtn.addEventListener("click", () => {
-    popup.classList.add("active");
-    overlay.classList.add("active");
-  });
+    openBtn.addEventListener("click", () => {
+        popup.classList.add("active");
+        overlay.classList.add("active");
+    });
 
-  closeBtn.addEventListener("click", () => {
-    popup.classList.remove("active");
-    overlay.classList.remove("active");
-  });
+    closeBtn.addEventListener("click", () => {
+        popup.classList.remove("active");
+        overlay.classList.remove("active");
+    });
 
-  overlay.addEventListener("click", () => {
-    popup.classList.remove("active");
-    overlay.classList.remove("active");
-  });
+    overlay.addEventListener("click", () => {
+        popup.classList.remove("active");
+        overlay.classList.remove("active");
+    });
 }
 
 setupPopup("openPopup1", "closePopup1", "popup1", "overlay1");
 setupPopup("openPopup2", "closePopup2", "popup2", "overlay2");
 
 function goToPage(page) {
-  window.location.href = page;
+    window.location.href = page;
 }
